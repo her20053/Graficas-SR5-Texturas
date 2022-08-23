e47ea3b3136cf5c0f2e9d95dc9ff9441585e2fcc
Joei <her20053@uvg.edu.gt>
2022-08-23 09:36:53 -0600

    Terminando texturas

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from gl import *
 # Se crea un canvas para poder dibujar:
 glInit(width=1024, height=1024)
 
-glLoadTexture(ObjectFileName='./assets/IGCG.obj',
-              textureFileName='./assets/IG.bmp')
+glLoadTexture(ObjectFileName='./assets/StarWars.obj',
+              textureFileName='./assets/StarWars.bmp')
 
 
 glInit(width=1024, height=1024)
-glApplyTexture(TextureBMPFileName='./assets/IG.bmp', objectFileName='./assets/IGCG.obj',
+glApplyTexture(TextureBMPFileName='./assets/IG.bmp', objectFileName='./assets/StarWars.obj',
                scaleFactor=(400, 400, 400), translateFactor=(500, 50, 0))
 nombreArchivo = 'SR5.bmp'
 glFinish(nombreArchivo)
